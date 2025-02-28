4758e352779c20b7cd50741c4edceeeab7fbb142
SharonTapiaD <22151132@aguascalientes.tecnm.mx>
2025-02-27 21:45:35 -0600

    Change Main Activity

--- app/src/main/java/com/example/workclass/MainActivity.kt
+++ app/src/main/java/com/example/workclass/MainActivity.kt
@@ -4,21 +4,68 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
+import com.example.workclass.ui.Screens.HomeScreen
 import com.example.workclass.ui.theme.WorkClassTheme
+import com.example.workclass.ui.Screens.MainMenuScreen
+import com.example.workclass.ui.Screens.TestScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            WorkClassTheme {
+                ComposableMultiScreenApp()
+            }
         }
+    }
+}
+
+@Composable
+fun ComposableMultiScreenApp(){
+    val navController = rememberNavController()
+    SetupNavGraph(navController = navController)
+}
+@Composable
+fun SetupNavGraph(navController: NavHostController){
+    NavHost(navController=navController, startDestination= "main_screen"){
+        composable("main_menu"){MainMenuScreen(navController) }
+        composable("home_screen"){HomeScreen(navController) }
+        composable("test_screen"){ TestScreen(navController) }
     }
 }
